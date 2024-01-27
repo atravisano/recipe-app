4430ff6e0db10520b638666b5930315aa8a85bee
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2024-01-27 14:42:09 -0500

    Refactor recipe fetch logic into Recipes function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import ApiService from './shared/services/ApiService';
 import Footer from './shared/components/Footer';
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
@@ -8,70 +7,30 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Recipes } from './shared/components/Recipes';
+import Recipes from './shared/components/Recipes';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    // Note: This may be called twice in development environment due to React.StrictMode.
-    new ApiService().getRecipes()
-      .then(
-        (result) => {
-          console.log(result);
-          this.setState({
-            isLoaded: true,
-            items: result.hits
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
   render() {
-    const { error, isLoaded, items } = this.state;
     const theme = createTheme();
-
-    if (error) {
-      return <div>{error}</div>
-    }
-    if (!isLoaded) {
-      return <div>Loading...</div>
-    }
     return (
       <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Recipes
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Recipes items={items}  />
-        </Container>
-      </main>
-      <Footer />
-    </ThemeProvider>
-  );
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Recipes
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            <Recipes />
+          </Container>
+        </main>
+        <Footer />
+      </ThemeProvider>
+    );
   }
 }
 
--- src/shared/components/Recipes.js
+++ src/shared/components/Recipes.js
@@ -4,28 +4,62 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import ApiService from '../services/ApiService';
+import { useEffect, useState } from 'react';
 
-export function Recipes(props) {
-    return <Grid container spacing={4}>
-      {props.items.map(item => item.recipe).map((item, index) => (
-        <Grid item key={index} xs={12} sm={6} md={4}>
-          <Card
-            sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-          >
-            <CardMedia
-              component="img"
-              image={item.image}
-              alt={item.label} />
-            <CardContent sx={{ flexGrow: 1 }}>
-              <Typography gutterBottom variant="h5" component="h2">
-                {item.label}
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                {Math.ceil(item.calories).toLocaleString()} Calories
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>;
+export default function Recipes() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [recipes, setRecipes] = useState([]);
+    
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+        // Note: This may be called twice in development environment due to React.StrictMode.
+        new ApiService().getRecipes()
+        .then(
+            (result) => {
+              setIsLoaded(true);
+              setRecipes(result.hits);
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              setIsLoaded(true);
+              setError(error);
+            }
+        )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        console.log(recipes)
+        return (
+            <Grid container spacing={4}>
+                {recipes.map(item => item.recipe).map((item, index) => (
+                    <Grid item key={index} xs={12} sm={6} md={4}>
+                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                        <CardMedia
+                            component="img"
+                            image={item.image}
+                            alt={item.label} />
+                        <CardContent sx={{ flexGrow: 1 }}>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {item.label}
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                {Math.ceil(item.calories).toLocaleString()} Calories
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                ))}
+            </Grid>
+        )
+      }
   }
