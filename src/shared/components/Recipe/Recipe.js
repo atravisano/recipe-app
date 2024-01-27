4cc6aefe4c3654c84fb82516e06fcb2d165dc111
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2024-01-27 14:50:26 -0500

    Refactor recipe template into new file

---
+++ src/shared/components/Recipe/Recipe.js
@@ -0,0 +1,23 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+export default function Recipe({ item }) {
+    return (
+        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <CardMedia
+                component="img"
+                image={item.image}
+                alt={item.label} />
+            <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {item.label}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    {Math.ceil(item.calories).toLocaleString()} Calories
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
--- src/shared/components/Recipes.js
+++ src/shared/components/Recipes.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
 import ApiService from '../services/ApiService';
 import { useEffect, useState } from 'react';
+import Recipe from './Recipe/Recipe';
 
 export default function Recipes() {
     const [error, setError] = useState(null);
@@ -42,21 +39,8 @@ export default function Recipes() {
         return (
             <Grid container spacing={4}>
                 {recipes.map(item => item.recipe).map((item, index) => (
-                    <Grid item key={index} xs={12} sm={6} md={4}>
-                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                        <CardMedia
-                            component="img"
-                            image={item.image}
-                            alt={item.label} />
-                        <CardContent sx={{ flexGrow: 1 }}>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                {item.label}
-                            </Typography>
-                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                                {Math.ceil(item.calories).toLocaleString()} Calories
-                            </Typography>
-                        </CardContent>
-                    </Card>
+                <Grid item key={index} xs={12} sm={6} md={4}>
+                    <Recipe item={item} />
                 </Grid>
                 ))}
             </Grid>
