06085c12277c911c6d29ca95d135b8407bd3afda
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2024-01-29 13:40:57 -0500

    Update to loading skeleton to roughly match width/height

--- src/recipes/components/RecipeItems/RecipeItems.jsx
+++ src/recipes/components/RecipeItems/RecipeItems.jsx
@@ -34,16 +34,13 @@ export default function RecipeItems() {
         return (
           <Grid container spacing={4}>
               <Grid item xs={12} sm={6} md={4}>
-                <Skeleton variant="rectangular" />
+                <Skeleton variant="rectangular" width={262} height={262} />
               </Grid>
               <Grid item xs={12} sm={6} md={4}>
-                <Skeleton variant="rectangular" />
+                <Skeleton variant="rectangular"  width={262} height={262}  />
               </Grid>
               <Grid item xs={12} sm={6} md={4}>
-                <Skeleton variant="rectangular" />
-              </Grid>
-              <Grid item xs={12} sm={6} md={4}>
-                <Skeleton variant="rectangular" />
+                <Skeleton variant="rectangular"  width={262} height={262}  />
               </Grid>
           </Grid>
         );
