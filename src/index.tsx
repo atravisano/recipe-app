340b41cfdeeaca88d62ce4cb22b16b14e0921427
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2024-01-29 14:41:01 -0500

    Update project to use typescript

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
