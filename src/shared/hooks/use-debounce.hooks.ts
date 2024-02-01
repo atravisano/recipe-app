0a23a237e71042fb06a2d42498a7efd192b18673
Anthony Travisano <A.R.Travisano@eagle.clarion.edu>
2024-02-01 16:17:56 -0500

    Update typing used in useDebounce

--- src/shared/hooks/use-debounce.hooks.ts
+++ src/shared/hooks/use-debounce.hooks.ts
@@ -2,7 +2,7 @@ import { debounce } from '@mui/material/utils';
 import { useEffect, useMemo, useRef } from 'react';
 
 export const useDebounce = (callback: () => void) => {
-    const ref = useRef<any>();
+    const ref = useRef<(typeof callback)>();
   
     useEffect(() => {
       ref.current = callback;
